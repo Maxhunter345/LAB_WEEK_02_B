10ce2099de8843a26a1915f0c28be04fa76b2f95
Mr Mekis <maxell.nathanael@gmail.com>
2025-09-05 21:31:08 +0700

    Commit No. 5 – Fix ResultActivity to allow resubmitting color code without app restart

--- app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -4,20 +4,34 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class ResultActivity : AppCompatActivity() {
     companion object {
-        private const val COLOR_KEY = "COLOR_KEY"
-        private const val ERROR_KEY = "ERROR_KEY"
+        const val COLOR_KEY = "COLOR_KEY"
+        const val ERROR_KEY = "ERROR_KEY"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
+        handleIntent(intent) // pertama kali activity dibuka
+        findViewById<Button>(R.id.back_button).setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)        // update intent lama ke intent baru
+        handleIntent(intent)     // handle ulang input terbaru
+    }
+
+    private fun handleIntent(intent: Intent) {
         val colorCode = intent.getStringExtra(COLOR_KEY)
 
         try {
@@ -32,7 +46,7 @@ class ResultActivity : AppCompatActivity() {
                 putExtra(ERROR_KEY, getString(R.string.color_code_input_invalid))
             }
             setResult(Activity.RESULT_CANCELED, data)
-            finish() // tutup ResultActivity
+            finish()
         }
     }
 }
